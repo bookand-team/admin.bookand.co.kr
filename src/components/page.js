d7e6b5f8ff6823421d3b5c9c787e5348ac22925e
YunSung-Choi97 <sunsu6765@gmail.com>
2022-10-19 13:26:49 +0900

    design: page 컴포넌트 스타일 변경

--- src/components/page.js
+++ src/components/page.js
@@ -1,17 +1,31 @@
+import Image from 'next/image';
+
+import leftArrowIcon from '../images/left_arrow_icon.svg';
+import leftArrow2Icon from '../images/left_arrow_icon2.svg';
+import rightArrowIcon from '../images/right_arrow_icon.svg';
+import rightArrow2Icon from '../images/right_arrow_icon2.svg';
 import styles from '../styles/page.module.css';
 
 const Page = () => {
   return (
     <nav className={styles.page}>
-      <button>{'<<'}</button>
-      <button>{'<'}</button>
-      <button className={styles.current}>{'1'}</button>
-      <button>{'2'}</button>
-      <button>{'3'}</button>
-      <button>{'4'}</button>
-      <button>{'5'}</button>
-      <button>{'>'}</button>
-      <button>{'>>'}</button>
+      <button className={styles.left2}>
+        <Image Image src={leftArrow2Icon} alt='left arrow icon' />
+      </button>
+      <button className={styles.left}>
+        <Image Image src={leftArrowIcon} alt='left arrow icon' />
+      </button>
+      <button className={styles.current}>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>4</button>
+      <button>5</button>
+      <button className={styles.right}>
+        <Image Image src={rightArrowIcon} alt='right arrow icon' />
+      </button>
+      <button className={styles.right2}>
+        <Image Image src={rightArrow2Icon} alt='right arrow icon' />
+      </button>
     </nav>
   );
 };
