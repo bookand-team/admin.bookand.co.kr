62540d1069bf0f00e0f723647882f71ff552066a
YunSung-Choi97 <sunsu6765@gmail.com>
2022-10-19 23:10:11 +0900

    feat: 파비콘 및 페이지 이름 설정

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,12 @@
 import Header from '../components/header';
+import Seo from '../components/seo';
 import wrapper from '../redux/store';
 import '../styles/global.css';
 
 const App = ({ Component, pageProps }) => {
   return (
     <>
+      <Seo />
       <Header />
       <Component {...pageProps} />
     </>
