287e35e7ef0b4b47d1a57951e924518a1486d750
YunSung-Choi97 <sunsu6765@gmail.com>
2022-10-25 06:01:52 +0900

    design: 대시보드탭 screen 크기에 따른 UI 적용, 표 레이아웃 통일

--- src/components/dashboard/current_situation.js
+++ src/components/dashboard/current_situation.js
@@ -15,7 +15,7 @@ const CurrentSituation = () => {
           <p>인터뷰 게시글 수: 00</p>
         </div>
         <div className={styles.section}>
-          <h4>맵</h4>
+          <h4>서점</h4>
           <p>API 등록서점 수: 00</p>
           <p>앱 노출서점 수: 00</p>
         </div>
@@ -37,6 +37,12 @@ const CurrentSituation = () => {
           <p>미노출 서점제보 수: 00</p>
           <p>오늘 접수된 피드백 수: 00</p>
           <p>이번주 접수된 피드백 수: 00</p>
+        </div>
+        <div className={styles.section}>
+          <h4>탈퇴관리</h4>
+          <p>총 탈퇴회원수: 00</p>
+          <p>Android 회원수: 00</p>
+          <p>IOS 회원수: 00</p>
         </div>
       </div>
     </div>
