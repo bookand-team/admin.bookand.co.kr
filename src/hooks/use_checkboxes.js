eeccd22cc014ea6d8004ff0a9eb47761bd7d0fcc
YunSung-Choi97 <sunsu6765@gmail.com>
2022-11-02 17:17:49 +0900

    comment: 커스텀훅 useCheckboxes 설명 추가

--- src/hooks/use_checkboxes.js
+++ src/hooks/use_checkboxes.js
@@ -1,5 +1,10 @@
 import { useCallback, useEffect, useState } from 'react';
 
+/**
+ * 현재 페이지의 체크된 박스들의 id값을 state로 관리
+ * @param {number} page 현재 페이지 번호
+ * @returns [checkBoxes, checkBoxHandler]: [체크된 박스 id값들 배열 state, 체크박스 state 변경함수(isChecked, boxId)]
+ */
 const useCheckBoxes = (page) => {
   // 체크된 박스들의 id값을 담는 리스트 state
   const [checkBoxes, setCheckBoxes] = useState([]);
