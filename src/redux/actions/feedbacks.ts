15c5d61ac24bc3898e79f3acb8cd961a83c98c36
YunSung-Choi97 <sunsu6765@gmail.com>
2023-01-11 20:14:08 +0900

    update: axios baseURL 설정 수정

--- src/redux/actions/feedbacks.ts
+++ src/redux/actions/feedbacks.ts
@@ -2,7 +2,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios, { AxiosError } from 'axios';
 
 // axios 기본 설정
-axios.defaults.baseURL = process.env.NEXT_PUBLIC_BACKEND_DOMAIN;
+axios.defaults.baseURL = `${process.env.NEXT_PUBLIC_AXIOS_PROTOCOL}${process.env.NEXT_PUBLIC_AXIOS_HOST}${process.env.NEXT_PUBLIC_AXIOS_COMMON_PATH}`;
 axios.defaults.withCredentials = true;  // front <-> backend 쿠키공유
 
 // 여러 피드백 조회하기
--- src/redux/actions/image.ts
+++ src/redux/actions/image.ts
@@ -2,7 +2,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios, { AxiosError } from 'axios';
 
 // axios 기본 설정
-axios.defaults.baseURL = process.env.NEXT_PUBLIC_BACKEND_DOMAIN;
+axios.defaults.baseURL = `${process.env.NEXT_PUBLIC_AXIOS_PROTOCOL}${process.env.NEXT_PUBLIC_AXIOS_HOST}${process.env.NEXT_PUBLIC_AXIOS_COMMON_PATH}`;
 axios.defaults.withCredentials = true;  // front <-> backend 쿠키공유
 
 // 이미지 url 생성하기
