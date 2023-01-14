350b91a76b570a3c7227748021a4b9ca6271fa77
YunSung-Choi97 <sunsu6765@gmail.com>
2023-01-15 01:48:13 +0900

    update: custom server > api routes 대체

--- server/utils/convert_json.js
+++
@@ -1,7 +0,0 @@
-let dataToConvert;
-
-const fs = require('fs');
-const path = require('path');
-
-const filePath = path.join(__dirname, 'report.json');
-fs.writeFileSync(filePath, JSON.stringify(dataToConvert, null, 2));
