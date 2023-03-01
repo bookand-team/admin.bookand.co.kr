7448b74b298e02135b624c898d46bceac1b590ff
YunSung-Choi97 <sunsu6765@gmail.com>
2023-03-01 18:19:45 +0900

    update: 로그인 요청과 토큰 저장 요청 분리

--- src/redux/actions/user.ts
+++ src/redux/actions/user.ts
@@ -2,13 +2,21 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import { isAxiosError } from 'axios';
 
 import { axiosBack, axiosFront } from '@config/axios';
-import { LoginReqDTO, SilentLoginReqDTO } from '@types';
+import { LoginReqDTO, SilentLoginReqDTO, isLoginSucRes } from '@types';
 
 // 로그인
 export const login = createAsyncThunk('login', async (data: LoginReqDTO, { rejectWithValue }) => {
   try {
-    const response = await axiosFront.post('/user/login', data);
-    return response.data;
+    const backRes = await axiosBack.post('/auth/admin', data);
+    const tokenData = backRes.data;
+
+    // 응답 데이터가 토큰 형식이 아닌 경우
+    if (!(tokenData && isLoginSucRes(tokenData))) {
+      return 'Error: token type error';
+    }
+    // 프론트서버에 토큰 저장 요청
+    await axiosFront.post('/user/store', tokenData);
+    return 'login success';
   } catch (error) {
     if (isAxiosError(error)) {
       return rejectWithValue(error.response ? error.response.data : error.message);
@@ -20,8 +28,16 @@ export const login = createAsyncThunk('login', async (data: LoginReqDTO, { rejec
 // 토큰 재발행
 export const silentLogin = createAsyncThunk('silentLogin', async (data: SilentLoginReqDTO, { rejectWithValue }) => {
   try {
-    const response = await axiosFront.post('/user/silent', data);
-    return response.data;
+    const backRes = await axiosBack.post('/auth/reissue', data);
+    const tokenData = backRes.data;
+
+    // 응답 데이터가 토큰 형식이 아닌 경우
+    if (!(tokenData && isLoginSucRes(tokenData))) {
+      return 'Error: token type error';
+    }
+    // 프론트서버에 토큰 저장 요청
+    await axiosFront.post('/user/store', tokenData);
+    return 'reissue success';
   } catch (error) {
     if (isAxiosError(error)) {
       return rejectWithValue(error.response ? error.response.data : error.message);
